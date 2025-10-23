3a645f10f8a467c7d6d9f247d44fc35a7d1e1078
MartinaDamian <martinadamian5151@gmail.com>
2025-10-23 10:57:05 -0500

    Clase pokenonContext

---
+++ ExamenP1/Models/PokemonContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExamenP1.Models
+{
+    public class PokemonContext
+    {
+        public DbSet<Pokemon> Pokemones { get; set; }
+        public DbSet<Entrenador> Entrenadores { get; set; }
+    }
+}
