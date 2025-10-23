3cbb3bb6e6423c99c76883685d995510010ac57b
Sara Chiriboga <sara.chiriboga.valle@udla.edu.ec>
2025-10-23 10:48:00 -0500

    Clase Pokemon

---
+++ ExamenP1/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenP1.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public int NivelPoder { get; set; }
+        public string Habilidad { get; set; }
+    }
+}
