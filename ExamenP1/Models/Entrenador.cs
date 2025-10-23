bbcd9b17a0487dab53da8f5ad93a323d4089ba87
nikinicole04 <129918152+nikinicole04@users.noreply.github.com>
2025-10-23 11:03:16 -0500

    Entrenador class
    Added properties for Id, NombreEntrenador, Region, Edad, and relationships with Pokemon.

--- ExamenP1/Models/Entrenador.cs
+++ ExamenP1/Models/Entrenador.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace ExamenP1.Models
 {
     public class Entrenador
     {
+
+        public int Id { get; set; }
+
+        public string NombreEntrenador { get; set; }
+
+        public string Region { get; set; }
+
+ 
+        public int Edad { get; set; }
+
+        // Relación con Pokémon
+    
+        public int PokemonId { get; set; }
+
+        public Pokemon Pokemon { get; set; }
     }
 }
